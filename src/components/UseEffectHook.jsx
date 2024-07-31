6c9a978d9c1adf9147bfe7d89ef50408fc1ec003
Samir Alam <samiralam@Wrong-Looper.local>
2024-07-31 08:41:29 +0545

    implement dep array demo in useeffect

--- src/components/UseEffectHook.jsx
+++ src/components/UseEffectHook.jsx
@@ -1,67 +1,77 @@
-// // demonstration of useEffect hook
+// demonstration of useEffect hook
+
+import React, { useState, useEffect } from "react";
+
+const Timer = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2>Timer</h2>
+      <p>Seconds: {count}</p>
+    </div>
+  );
+};
+
+const UseEffectHook = () => {
+  // const Timer = () => {
+
+  // }
+  return (
+    <div>
+      <h1>useEffect Hook</h1>
+      <Timer />
+    </div>
+  );
+};
+
+export default UseEffectHook;
 
 // import React, { useState, useEffect } from "react";
 
-// const Timer = () => {
+// const UseEffectHook = () => {
 //   const [count, setCount] = useState(0);
 
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       setCount((prevCount) => prevCount + 1);
-//     }, 1000);
+//   const clickHandler = () => setCount(count + 1);
 
-//     return () => {
-//       clearInterval(interval);
-//     };
-//   }, []);
+//   // useEffect(() => {
+//   //   console.log("useEffect called");
+//   //   document.title = `Count: ${count}`;
+//   // }, [5]);
+
+//   // useEffect(() => {
+//   //   console.log("useEffect called");
+//   //   document.title = `Count: ${count}`;
+//   // }, [count]);
+
+//   // useEffect(() => {
+//   //   console.log("useEffect called");
+//   //   document.title = `Count: ${count}`;
+//   // }, []);
+
+//   // useEffect(() => {
+//   //   console.log("useEffect called");
+//   //   document.title = `Count: ${count}`;
+//   // }, [1]);
 
 //   return (
 //     <div>
-//       <h2>Timer</h2>
-//       <p>Seconds: {count}</p>
-//     </div>
-//   );
-// };
+//       <div>Use Effect demo</div>
+//       <div>Count: {count}</div>
 
-// const UseEffectHook = () => {
-//   // const Timer = () => {
-
-//   // }
-//   return (
-//     <div>
-//       <h1>useEffect Hook</h1>
-//       <Timer />
+//       <button onClick={clickHandler}>Increment</button>
 //     </div>
 //   );
 // };
 
 // export default UseEffectHook;
-
-import { useState, useEffect } from "react";
-
-const UseEffectHook = () => {
-  const [count, setCount] = useState(0);
-
-  const clickHandler = () => setCount(count + 1);
-
-  // useEffect(() => {
-  //   console.log("useEffect called");
-  //   document.title = `Count: ${count}`;
-  // }, [5]);
-
-  useEffect(() => {
-    console.log("useEffect called");
-    document.title = `Count: ${count}`;
-  }, [count]);
-
-  return (
-    <>
-      <div>Use Effect demo</div>
-      <div>Count: {count}</div>
-
-      <button onClick={clickHandler}>Increment</button>
-    </>
-  );
-};
-
-export default UseEffectHook;
